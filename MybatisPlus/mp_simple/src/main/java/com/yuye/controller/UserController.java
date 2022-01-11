31498a4d637fced0618d0c91e7d565c08733d6ee
yuye <962701261@qq.com>
2022-01-11 21:32:53 +0800

    mybatisplus分页查询

--- MybatisPlus/mp_simple/src/main/java/com/yuye/controller/UserController.java
+++ MybatisPlus/mp_simple/src/main/java/com/yuye/controller/UserController.java
@@ -2,6 +2,9 @@ package com.yuye.controller;
 
 import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yuye.mapper.UserMapper;
 import com.yuye.pojo.User;
 import org.apache.ibatis.io.Resources;
@@ -18,6 +21,66 @@ import java.util.List;
 
 
 public class UserController {
+    @Test
+    public void insert() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setUserName("tom");
+        user.setPassword("123456");
+        user.setName("汤姆");
+        user.setAge(18);
+        user.setMail("111113@qq.com");
+        user.setAddress("北京");
+        int insert = mapper.insert(user);
+        Long userId = user.getId();
+        System.out.println(insert);
+        sqlSession.commit();
+        System.out.println(userId);
+    }
+    @Test
+    public void update2() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        userUpdateWrapper.set("user_name","zhaoliu").set("age",18).eq("user_id",4L);//set设置的是数据库中的字段，不是属性名
+        int i = mapper.update(null,userUpdateWrapper);
+        sqlSession.commit();
+        System.out.println(i);
+    }
+    @Test
+    public void update() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setUserName("lucy");
+        user.setAge(20);
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<User>();
+        userQueryWrapper.eq("user_id",4);
+        int i = mapper.update(user,userQueryWrapper);
+        sqlSession.commit();
+        System.out.println(i);
+    }
+    @Test
+    public void updateById() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setUserName("lucy");
+        user.setAge(18);
+        user.setId(4L);
+        int i = mapper.updateById(user);
+        sqlSession.commit();
+        System.out.println(i);
+    }
     @Test
     public void delete() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
@@ -112,25 +175,7 @@ public class UserController {
         User user = mapper.selectById(3L);
         System.out.println(user);
     }
-    @Test
-    public void insert() throws IOException {
-        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        User user = new User();
-        user.setUserName("tom");
-        user.setPassword("123456");
-        user.setName("汤姆");
-        user.setAge(18);
-        user.setMail("111113@qq.com");
-        user.setAddress("北京");
-        int insert = mapper.insert(user);
-        Long userId = user.getId();
-        System.out.println(insert);
-        sqlSession.commit();
-        System.out.println(userId);
-    }
+
     @Test
     public void selectList() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
@@ -142,6 +187,29 @@ public class UserController {
             System.out.println(user);
         }
     }
+
+    @Test
+    public void selectPage() throws IOException {
+
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new MybatisSqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Page<User> userPage1 = new Page<User>(3,1);
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.like("password","1");
+        IPage<User> userPage = mapper.selectPage(userPage1, userQueryWrapper);
+        System.out.println("分页总页数:"+userPage.getPages());
+        System.out.println("总条数:"+userPage.getTotal());
+        System.out.println("当前页面:"+userPage.getCurrent());
+        System.out.println("当前页面条数:"+userPage.getSize());
+        List<User> records = userPage.getRecords();
+        for (User record : records) {
+            System.out.println("页面数据"+record);
+        }
+    }
+
+
     @Test
     public void findAll() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
--- MybatisPlus/mp_simple/src/main/java/com/yuye/mapper/UserMapper.java
+++ MybatisPlus/mp_simple/src/main/java/com/yuye/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package com.yuye.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yuye.pojo.User;
 
 import java.util.List;
 
 public interface UserMapper extends BaseMapper<User> {
     List<User> findAll();
+    IPage<User> selectPageVo(Page<?> page, Integer state);
 }
